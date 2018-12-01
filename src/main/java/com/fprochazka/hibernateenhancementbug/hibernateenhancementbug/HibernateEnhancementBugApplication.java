ba3ed4c08f089a5738d192b39849074acab5e9e9
Filip Procházka <filip@prochazka.su>
2018-12-01 16:54:22 +0100

    clean spring boot project

---
+++ src/main/java/com/fprochazka/hibernateenhancementbug/hibernateenhancementbug/HibernateEnhancementBugApplication.java
@@ -0,0 +1,12 @@
+package com.fprochazka.hibernateenhancementbug.hibernateenhancementbug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateEnhancementBugApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(HibernateEnhancementBugApplication.class, args);
+    }
+}
